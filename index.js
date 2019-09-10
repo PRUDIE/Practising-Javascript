30e1d59c072195447554ac2db30974262edfa0a0
PRUDIE <32335862+PRUDIE@users.noreply.github.com>
2019-09-11 00:03:14 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const kelvin = 298
+//const is used to declare variables that can't be changed
+let celsius;
+//creating a variable called celsius and then assigning it parameters. kelvin to celsius conversion
+celsius = kelvin -273
+let fahrenheit
+//this line explains the conversion to fahrenheit
+fahrenheit = celsius * (9/5) + 32
+//print to the least whole number
+console.log(Math.floor(fahrenheit))
+console.log('The temperature is TEMPERATURE degrees Fahrenheit' )
+
